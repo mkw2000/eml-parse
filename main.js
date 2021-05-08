fdc88d10e77df8d0c51ea5e9b4f2fa9563ab2fa7
mkw <michaelkweiner@gmail.com>
2021-05-08 11:17:40 -0700

    update

--- main.js
+++ main.js
@@ -6,38 +6,37 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 let contacts = [];
 
-module.exports = function() {
-    
-    fs.writeFile('emails.csv','', function (err) {
-        if (err) return console.log(err);
+
+fs.writeFile('emails.csv','', function (err) {
+    if (err) return console.log(err);
+});
+const csvWriter = createCsvWriter({
+    path: 'emails.csv',
+    header: [
+        {id: 'name', title: 'NAME'},
+        {id: 'address', title: 'ADDRESS'}
+    ]
+});
+
+glob(__dirname + '/**/*.eml', {}, (err, files)=>{
+    async.each(files, function(file, callback) {
+        let emailFile = new EmlParser(fs.createReadStream(file)).parseEml().then(result => {
+            contacts.push(result.from.value[0])
+            callback();
+        })
+    }, function(err) {
+        if( err ) {
+            console.log('Process Failed');
+        } else {
+            csvWriter.writeRecords(contacts)       
+            .then(() => {
+                console.log('Done Writing CSV');
+            });
+            console.log('Process Complete');
+        }
     });
-    const csvWriter = createCsvWriter({
-        path: 'emails.csv',
-        header: [
-            {id: 'name', title: 'NAME'},
-            {id: 'address', title: 'ADDRESS'}
-        ]
-    });
-    
-    glob(__dirname + '/**/*.eml', {}, (err, files)=>{
-        async.each(files, function(file, callback) {
-            let emailFile = new EmlParser(fs.createReadStream(file)).parseEml().then(result => {
-                contacts.push(result.from.value[0])
-                callback();
-            })
-        }, function(err) {
-            if( err ) {
-                console.log('Process Failed');
-            } else {
-                csvWriter.writeRecords(contacts)       
-                .then(() => {
-                    console.log('Done Writing CSV');
-                });
-                console.log('Process Complete');
-            }
-        });
-    })
-}
+})
+
 
 
 
